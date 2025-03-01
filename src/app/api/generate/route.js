026b5c04973c9b3bca21c0535755f95a275569b2
Harsh chaudhary <146811538+piyush1740x@users.noreply.github.com>
2025-03-02 03:58:39 +0530

    fourth

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -12,7 +12,7 @@ export async function POST(req) {
         const genAI = new GoogleGenerativeAI(apiKey);
         const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-        const prompt = `Write a ${mood} love letter in hinglish for my girlfriend named ${herName} and my name is ${yourName}. dont write "Okay, here's a romantic love letter in Hinglish for Payal from Piyush:" this lind of thing just start writing letter `;
+        const prompt = `Write a ${mood} love letter in hinglish for my girlfriend named ${herName} and my name is ${yourName}.with emogis, and format it properly, dont write "Okay, here's a romantic love letter in Hinglish for Payal from Piyush:" this lind of thing just start writing letter `;
         const result = await model.generateContent(prompt);
         const responseText = await result.response.text();
 
